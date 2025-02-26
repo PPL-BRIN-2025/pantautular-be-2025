f2ef96956addc45eb7f49ad10e6e7c6fe4fbe97e
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-02-26 15:52:53 +0700

    Get all locations repository (empty and exceptions) - Green

--- pt_backend/repositories.py
+++ pt_backend/repositories.py
@@ -1,16 +1,22 @@
-from .models import Case, Location
+from .models import Case
+from django.core.exceptions import ObjectDoesNotExist
 
 class CaseRepository:
     def get_all_case_locations(self):
-        locations = Case.get_all_cases_locations()
-        return [
-            {
-                "id": str(location.case.id),
-                "city": location.case.city,
-                "latitude": f"{float(location.latitude):.6f}", 
-                "longitude": f"{float(location.longitude):.6f}",  
-            }
-            for location in locations
-        ]
+        try:
+            locations = Case.get_all_cases_locations()
+            if not locations.exists():
+                return []
+            return [
+                {
+                    "id": str(location.case.id),
+                    "city": location.case.city,
+                    "latitude": f"{float(location.latitude):.6f}",
+                    "longitude": f"{float(location.longitude):.6f}",
+                }
+                for location in locations
+            ]
+        except ObjectDoesNotExist:  
+            return {"error": "Error retrieving case locations"}
 
     
