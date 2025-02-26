3b43e19d3ebda1e65a15e4605fa6d7abfa6ba09d
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-02-26 15:48:42 +0700

    Get all locations API - Green

--- pt_backend/serializers.py
+++ pt_backend/serializers.py
@@ -1,2 +1,10 @@
 from rest_framework import serializers
 from .models import Case
+
+class CaseLocationSerializer(serializers.ModelSerializer):
+    latitude = serializers.DecimalField(source="locations.latitude", max_digits=8, decimal_places=6)
+    longitude = serializers.DecimalField(source="locations.longitude", max_digits=9, decimal_places=6)
+
+    class Meta:
+        model = Case
+        fields = ["id", "city", "latitude", "longitude"]
--- pt_backend/views.py
+++ pt_backend/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .repositories import CaseRepository
+
+class AllCaseLocationsView(APIView):
+    def get(self, request):
+        repository = CaseRepository()
+        locations = repository.get_all_case_locations()
+        return Response(locations, status=status.HTTP_200_OK)
 
 
 @require_GET
