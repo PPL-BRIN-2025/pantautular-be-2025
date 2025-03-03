db96345a7456d43d26c2099e6bcc228464e8d08a
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2025-03-03 11:19:45 +0700

    [GREEN]: implement Filters View

--- filter/views.py
+++ filter/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from pt_backend.repositories import DiseaseRepository, LocationRepository, NewsRepository
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+class FiltersView(APIView):
+    def get(self, request):
+        disease_repository = DiseaseRepository()
+        location_repository = LocationRepository()
+        news_repository = NewsRepository()
+        try:
+            diseases = disease_repository.get_all_diseases_name()
+            locations = location_repository.get_all_locations_name()
+            news = news_repository.get_all_news_name()
+            return Response(
+                {
+                    "diseases": diseases,
+                    "locations": locations,
+                    "news": news
+                },
+                status=status.HTTP_200_OK
+            )
+        except ObjectDoesNotExist:
+            return Response({"error": "No diseases found"}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
