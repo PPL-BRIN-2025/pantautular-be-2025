e4b33b1135c34b76e02d87f76a0315184a90d140
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-03 14:15:03 +0700

    Refactoring API View -RED

--- pt_backend/views.py
+++ pt_backend/views.py
@@ -1,18 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .repositories import CaseRepository
 from django.core.exceptions import ObjectDoesNotExist
 
 class AllCaseLocationsView(APIView):
-    def get(self, request):
-        repository = CaseRepository()
-        try:
-            locations = repository.get_all_case_locations()
-            return Response(locations, status=status.HTTP_200_OK)
-        except ObjectDoesNotExist:
-            return Response({"error": "No case locations found"}, status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    pass
 
 
