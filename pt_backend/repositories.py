cda2320b079442d8d36152506c390d31ade0dbea
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-04 20:14:53 +0700

    Case Repository: Get all case locations - RED

--- pt_backend/repositories.py
+++ pt_backend/repositories.py
@@ -4,11 +4,5 @@ from django.core.exceptions import ObjectDoesNotExist
 class CaseRepository:
     @staticmethod
     def get_all_case_locations():
-        try:
-            locations = Case.get_all_cases_locations()
-            if not locations.exists():
-                return []
-            return locations
-        except ObjectDoesNotExist:
-            return None
+        pass
     
--- pt_backend/tests.py
+++ pt_backend/tests.py
@@ -7,6 +7,28 @@ from django.core.exceptions import ObjectDoesNotExist
 import uuid
 from unittest.mock import patch
 
+class CaseRepositoryTestCase(TestCase):
+    def setUp(self):
+        self.disease = Disease.objects.create(name="COVID-19", level_of_alertness=5)
+        self.location = Location.objects.create(
+            latitude=-6.9175, longitude=107.6191, name="Bandung"
+        )
+        self.case = Case.objects.create(
+            id=uuid.uuid4(),
+            gender="Female",
+            age=25,
+            city="Bandung",
+            status="recovered",
+            disease=self.disease,
+            location=self.location
+        )
 
-
+    def test_get_all_case_locations(self):
+        locations = CaseRepository.get_all_case_locations()
+        self.assertTrue(locations.exists())
+        self.assertEqual(locations.count(), 1)
+        case_data = locations.first()
+        self.assertEqual(str(case_data["id"]), str(self.case.id))
+        self.assertEqual(float(case_data["longitude"]), -6.9175)
+        self.assertEqual(float(case_data["latitude"]), 107.6191)
     
