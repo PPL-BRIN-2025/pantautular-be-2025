6de3ebb7ba8674b02b96c7f777e65a439e09c5ef
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-04 22:14:38 +0700

    Case

--- pt_backend/tests.py
+++ pt_backend/tests.py
@@ -41,4 +41,29 @@ class CaseRepositoryTestCase(TestCase):
     def test_get_all_case_locations_handles_object_does_not_exist(self, mock_get_all_cases_locations):
         locations = CaseRepository.get_all_case_locations()
         self.assertIsNone(locations)
+
+class CaseAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.disease1 = Disease.objects.create(name="Flu", level_of_alertness=2)
+        self.disease2 = Disease.objects.create(name="COVID-19", level_of_alertness=5)
+        self.location1 = Location.objects.create(latitude=-6.2088, longitude=106.8456, name="Jakarta")
+        self.location2 = Location.objects.create(latitude=-6.9175, longitude=107.6191, name="Bandung")
+        self.case1 = Case.objects.create(
+            id=uuid.uuid4(), gender="Male", age=30, city="Jakarta", status="confirmed", disease=self.disease1, location=self.location1
+        )
+        self.case2 = Case.objects.create(
+            id=uuid.uuid4(), gender="Female", age=25, city="Bandung", status="recovered", disease=self.disease2, location=self.location2
+        )
+
+
+    def test_get_all_case_locations(self):
+        response = self.client.get('/cases/locations/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), [
+            {"id": str(self.case1.id), "city": "Jakarta", "latitude": "-6.208800", "longitude": "106.845600"},
+            {"id": str(self.case2.id), "city": "Bandung", "latitude": "-6.917500", "longitude": "107.619100"}
+        ])
+
     
