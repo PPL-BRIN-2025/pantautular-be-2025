fb2ee8d670d483edc1923722e26122f2959da377
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-05 18:46:03 +0700

    Add API Security: -RED

--- pantau_tular/settings.py
+++ pantau_tular/settings.py
@@ -26,6 +26,7 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_API_KEY = os.getenv('SECRET_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
