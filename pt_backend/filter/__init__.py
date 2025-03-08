e1d2bc9cf2dbe9ba62b49d33fd5bac1bc6f23cf5
vinkakniv <vinka.aeris@gmail.com>
2025-03-09 00:28:21 +0700

    [GREEN] Add FilterStrategy protocol for query building

---
+++ pt_backend/filter/__init__.py
@@ -0,0 +1,17 @@
+from .strategy import FilterStrategy
+from .filters import (
+    DiseaseFilter,
+    LocationFilter,
+    AlertnessFilter,
+    PortalFilter,
+    DateRangeFilter
+)
+
+__all__ = [
+    'FilterStrategy',
+    'DiseaseFilter',
+    'LocationFilter',
+    'AlertnessFilter',
+    'PortalFilter',
+    'DateRangeFilter'
+] 
