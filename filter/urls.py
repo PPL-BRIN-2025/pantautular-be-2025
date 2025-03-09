5f3c6eebd32d9ca89b0d75cddc9f4080a1645bb1
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2025-03-10 01:16:19 +0700

    [REFACTOR]: delete filter application

--- filter/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import FiltersView
-
-urlpatterns = [
-    path('api/filters/', FiltersView.as_view(), name='filters'),
-]
--- pantau_tular/urls.py
+++ pantau_tular/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pt_backend.urls')),
-    path('', include('filter.urls')),
 ]
 
