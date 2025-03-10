f6007c186e1bc9f88a98d521dee00ae61a0fa04f
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2025-03-10 12:34:54 +0700

    [REFACTOR]: fix redundancy

--- pt_backend/tests/test_repository.py
+++ pt_backend/tests/test_repository.py
@@ -7,14 +7,31 @@ from django.core.exceptions import ObjectDoesNotExist
 import uuid
 from unittest.mock import patch
 
-class DiseaseRepositoryTestCase(TestCase):
+class BaseTestCase(TestCase):
     def setUp(self):
-        self.disease1 = Disease.objects.create(
-            id=uuid.uuid4(), name="COVID-19", level_of_alertness=5
+        self.disease1 = Disease.objects.create(id=uuid.uuid4(), name="COVID-19", level_of_alertness=5)
+        self.disease2 = Disease.objects.create(id=uuid.uuid4(), name="Ebola", level_of_alertness=4)
+
+        self.location1 = Location.objects.create(id=uuid.uuid4(), latitude=-6.2088, longitude=106.8456, name="Jakarta")
+        self.location2 = Location.objects.create(id=uuid.uuid4(), latitude=-6.9175, longitude=107.6191, name="Bandung")
+
+        self.case1 = Case.objects.create(
+            id=uuid.uuid4(), gender="Pria", age=30, city="Jakarta", status="kematian", disease=self.disease1, location=self.location1
         )
-        self.disease2 = Disease.objects.create(
-            id=uuid.uuid4(), name="Ebola", level_of_alertness=4
+        self.case2 = Case.objects.create(
+            id=uuid.uuid4(), gender="Wanita", age=25, city="Bandung", status="terjangkit", disease=self.disease2, location=self.location2
         )
+
+        self.news1 = News.objects.create(
+            id=uuid.uuid4(), portal="kompas.com", type="health", title="COVID-19 Detected in Jakarta", content="COVID-19 case detected in Jakarta...", url="https://www.kompas.com/covid-jakarta", author="Dr. Joko", case=self.case1
+        )
+        self.news2 = News.objects.create(
+            id=uuid.uuid4(), portal="detik.com", type="health", title="SARS Detected in Medan", content="SARS case detected in Medan...", url="https://www.detik.com/sars-medan", author="Dr. Sari", case=self.case2
+        )
+
+class DiseaseRepositoryTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
         self.repository = DiseaseRepository()
 
     def test_get_all_diseases_name(self):
@@ -33,15 +50,9 @@ class DiseaseRepositoryTestCase(TestCase):
         result = self.repository.get_all_diseases_name()
         self.assertEqual(result, {"error": "Error retrieving diseases"})
 
-class LocationRepositoryTestCase(TestCase):
+class LocationRepositoryTestCase(BaseTestCase):
     def setUp(self):
-        self.disease = Disease.objects.create(name="COVID-19", level_of_alertness=5)
-        self.location1 = Location.objects.create(
-            latitude=-6.2088, longitude=106.8456, name="Jakarta"
-        )
-        self.location2 = Location.objects.create(
-            latitude=-6.9175, longitude=107.6191, name="Bandung"
-        )
+        super().setUp()
         self.repository = LocationRepository()
 
     def test_get_all_locations_name(self):
@@ -60,53 +71,9 @@ class LocationRepositoryTestCase(TestCase):
         result = self.repository.get_all_locations_name()
         self.assertEqual(result, {"error": "Error retrieving locations"})
 
-class NewsRepositoryTestCase(TestCase):
+class NewsRepositoryTestCase(BaseTestCase):
     def setUp(self):
-        self.disease1 = Disease.objects.create(id=uuid.uuid4(), name="COVID-19", level_of_alertness=5)
-        self.disease2 = Disease.objects.create(id=uuid.uuid4(), name="Ebola", level_of_alertness=4)
-
-        self.location1 = Location.objects.create(id=uuid.uuid4(), latitude=-6.2088, longitude=106.8456, name="Jakarta")
-        self.location2 = Location.objects.create(id=uuid.uuid4(), latitude=-6.9175, longitude=107.6191, name="Bandung")
-
-        self.case1 = Case.objects.create(
-            id=uuid.uuid4(),
-            gender="Pria",
-            age=30,
-            city="Jakarta",
-            status="kematian",
-            disease=self.disease1,
-            location=self.location1
-        )
-        self.case2 = Case.objects.create(
-            id=uuid.uuid4(),
-            gender="Wanita",
-            age=25,
-            city="Bandung",
-            status="terjangkit",
-            disease=self.disease2,
-            location=self.location2
-        )
-
-        self.news1 = News.objects.create(
-            id=uuid.uuid4(),
-            portal="kompas.com",
-            type="health",
-            title="COVID-19 Detected in Jakarta",
-            content="COVID-19 case detected in Jakarta...",
-            url="https://www.kompas.com/covid-jakarta",
-            author="Dr. Joko",
-            case=self.case1
-        )
-        self.news2 = News.objects.create(
-            id=uuid.uuid4(),
-            portal="detik.com",
-            type="health",
-            title="SARS Detected in Medan",
-            content="SARS case detected in Medan...",
-            url="https://www.detik.com/sars-medan",
-            author="Dr. Sari",
-            case=self.case2
-        )
+        super().setUp()
         self.repository = NewsRepository()
 
     def test_get_all_news_name(self):
