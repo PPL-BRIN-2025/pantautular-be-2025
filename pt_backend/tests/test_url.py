0a81a42b46368a412fd79f08e71230c6b8f5ae29
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2025-03-11 11:26:04 +0700

    [REFACTOR]: solve sonar redundancy on test_url, test_views

--- pt_backend/tests/test_url.py
+++ pt_backend/tests/test_url.py
@@ -5,25 +5,21 @@ from unittest.mock import patch
 from .test_repository import BaseTestCase
 
 class FilterAPITest(BaseTestCase):
+    def _assert_response_data(self, response_data, expected_data):
+        for key in expected_data:
+            for item in response_data[key]:
+                self.assertIn(item, expected_data[key])
+            self.assertEqual(len(response_data[key]), len(expected_data[key]))
+
     def test_get_filters(self):
         response = self.client.get('/api/filters/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        response_data = response.json()
-        expected_diseases = ["COVID-19", "Ebola"]
-        expected_locations = ["Jakarta", "Bandung"]
-        expected_news = ["kompas.com", "detik.com"]
-
-        for disease in response_data["diseases"]:
-            self.assertIn(disease, expected_diseases)
-        self.assertEqual(len(response_data["diseases"]), len(expected_diseases))
-        
-        for location in response_data["locations"]:
-            self.assertIn(location, expected_locations)
-        self.assertEqual(len(response_data["locations"]), len(expected_locations))
-        
-        for news in response_data["news"]:
-            self.assertIn(news, expected_news)
-        self.assertEqual(len(response_data["news"]), len(expected_news))
+        expected_data = {
+            "diseases": ["COVID-19", "Ebola"],
+            "locations": ["Jakarta", "Bandung"],
+            "news": ["kompas.com", "detik.com"]
+        }
+        self._assert_response_data(response.json(), expected_data)
     
     def test_get_filters_empty(self):
         Disease.objects.all().delete()
--- pt_backend/tests/test_views.py
+++ pt_backend/tests/test_views.py
@@ -1,11 +1,15 @@
 from rest_framework.test import APIClient
 from rest_framework import status
 from pt_backend.tests.test_repository import BaseTestCase
-from rest_framework.test import APIClient
-from rest_framework import status
 
 
 class FiltersViewTest(BaseTestCase):
+    def _assert_response_data(self, response_data, expected_data):
+        for key in expected_data:
+            for item in response_data[key]:
+                self.assertIn(item, expected_data[key])
+            self.assertEqual(len(response_data[key]), len(expected_data[key]))
+
     def setUp(self):
         super().setUp()
         self.client = APIClient()
@@ -13,20 +17,9 @@ class FiltersViewTest(BaseTestCase):
     def test_get_filters(self):
         response = self.client.get('/api/filters/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
-        response_data = response.json()
-        expected_diseases = ["COVID-19", "Ebola"]
-        expected_locations = ["Jakarta", "Bandung"]
-        expected_news = ["kompas.com", "detik.com"]
-
-        for disease in response_data["diseases"]:
-            self.assertIn(disease, expected_diseases)
-        self.assertEqual(len(response_data["diseases"]), len(expected_diseases))
-        
-        for location in response_data["locations"]:
-            self.assertIn(location, expected_locations)
-        self.assertEqual(len(response_data["locations"]), len(expected_locations))
-        
-        for news in response_data["news"]:
-            self.assertIn(news, expected_news)
-        self.assertEqual(len(response_data["news"]), len(expected_news))
+        expected_data = {
+            "diseases": ["COVID-19", "Ebola"],
+            "locations": ["Jakarta", "Bandung"],
+            "news": ["kompas.com", "detik.com"]
+        }
+        self._assert_response_data(response.json(), expected_data)
