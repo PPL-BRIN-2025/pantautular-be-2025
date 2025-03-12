cebc1b53126217acccca4c4c61253a6e0695cc46
vinkakniv <vinka.aeris@gmail.com>
2025-03-13 03:50:14 +0700

    [REFACTOR] Change logic in date range filter

--- pt_backend/filter/date_range_filter.py
+++ pt_backend/filter/date_range_filter.py
@@ -1,27 +1,31 @@
 from datetime import datetime
 import pytz
 from django.db.models import Q
-from typing import Dict
-from typing import Optional
+from typing import Dict, Optional
 
 class DateRangeFilter:
     def apply(self, data: Dict) -> Q:
-        if "start_date" not in data or "end_date" not in data:
-            return Q()
-        
         utc = pytz.UTC
-        start_date = self.parse_datetime(data["start_date"], utc)
-        end_date = self.parse_datetime(data["end_date"], utc)
 
-        if not start_date or not end_date:
-            return Q() 
+        start_date = self.parse_datetime(data.get("start_date"), utc)
+        end_date = self.parse_datetime(data.get("end_date"), utc)
 
-        return Q(news__date_published__range=[start_date, end_date]) & Q(news__isnull=False)
+        if start_date and end_date:
+            return Q(news__date_published__range=[start_date, end_date]) & Q(news__isnull=False)
+        elif start_date:
+            return Q(news__date_published__gte=start_date) & Q(news__isnull=False)
+        elif end_date:
+            return Q(news__date_published__lte=end_date) & Q(news__isnull=False)
+        else:
+            return Q()
 
-    def parse_datetime(self, date_str: str, timezone) -> Optional[datetime]:
+    def parse_datetime(self, date_str: Optional[str], timezone) -> Optional[datetime]:
+        if not date_str:  # Handle None case
+            return None
+        
         formats = [
-            "%Y-%m-%dT%H:%M:%S.%fZ",  # Format ISO 8601 dengan milidetik
-            "%Y-%m-%dT%H:%M:%SZ",      # Format ISO 8601 tanpa milidetik
+            "%Y-%m-%dT%H:%M:%S.%fZ",  # ISO 8601 dengan milidetik
+            "%Y-%m-%dT%H:%M:%SZ",      # ISO 8601 tanpa milidetik
             "%Y-%m-%d %H:%M:%S",       # Format biasa dengan spasi
         ]
         for fmt in formats:
