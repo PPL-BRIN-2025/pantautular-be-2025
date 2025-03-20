b7589fda3df098cec7c99e5fbb4647fda5e79209
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2025-03-20 08:32:20 +0700

    [REFACTOR]add serializers for GenderDistribution

--- pt_backend/serializers.py
+++ pt_backend/serializers.py
@@ -6,3 +6,6 @@ class CaseLocationSerializer(serializers.Serializer):
     location__latitude = serializers.DecimalField(max_digits=8, decimal_places=6)
     city = serializers.CharField(max_length=255)
 
+class GenderDistributionSerializer(serializers.Serializer):
+    male = serializers.CharField(max_length=10)
+    female = serializers.CharField(max_length=10)
