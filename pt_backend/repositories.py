13ea17e34aeb53f4e9b2c1de281d591d510720e9
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-21 02:33:00 +0700

    Implement Get all Cases -GREEN

--- pt_backend/repositories.py
+++ pt_backend/repositories.py
@@ -5,7 +5,8 @@ class CaseRepository(CaseRepositoryInterface):
     def get_all_locations(self):
         return Case.get_all_locations()
     
-    
+    def get_all_cases(self):
+        return Case.objects.all()
     
     
     
