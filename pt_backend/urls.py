c320a82bac787d297647c8421b50cc90c3a15bf4
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2025-03-21 15:22:57 +0700

    [GREEN]: Add URL route for SeverityDatesView to retrieve severity dates

--- pt_backend/urls.py
+++ pt_backend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AllCaseLocationsView, FiltersView
+from .views import AllCaseLocationsView, FiltersView, SeverityDatesView
 
 urlpatterns = [
     path('cases/locations/', AllCaseLocationsView.as_view(), name='all-case-locations'),
     path('api/filters/', FiltersView.as_view(), name='filters'),
+    path('api/severity-dates/', SeverityDatesView.as_view(), name='severity-dates'),
 ]
