b3f2e92288bbb10a057bc3fdc125fd5031c85aeb
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-21 22:29:28 +0700

    Refactor CaseRepository - get_all_cases(Refactored Tests) -GREEN

--- pt_backend/repositories.py
+++ pt_backend/repositories.py
@@ -1,4 +1,16 @@
+from .models import Case
+from .interfaces import CaseRepositoryInterface
 
+class CaseRepository(CaseRepositoryInterface):
+    def get_all_cases(self):
+        return Case.objects.all().values(
+            "id",
+            "location__province",
+            "location__city",
+            "news__portal",
+            "severity",
+            "news__date_published"
+        )
     
     
 
