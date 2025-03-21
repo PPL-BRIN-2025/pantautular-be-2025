aea5af4dbca8194314b760a45553541626f29ea2
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-21 23:14:37 +0700

    Remove unused codes

--- pt_backend/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import AllCaseLocationsView
-
-urlpatterns = [
-    path('cases/locations/', AllCaseLocationsView.as_view(), name='all-case-locations')
-]
--- pt_backend/views.py
+++ pt_backend/views.py
@@ -1,33 +1,3 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .serializers import CaseLocationSerializer
-from .services import CacheService, CaseService
-from .repositories import CaseRepository
-from .authentication import APIKeyAuthentication
-
-class AllCaseLocationsView(APIView):
-    authentication_classes = [APIKeyAuthentication]
-    permission_classes = []
-    
-    serializer_class = CaseLocationSerializer
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        cache_service = CacheService()
-        repository = CaseRepository()
-        self.service = CaseService(repository, cache_service)  
-
-    def get(self, request):
-        try:
-            cases = self.service.get_all_case_locations()
-            if not cases:
-                return Response({"error": "No case locations found"}, status=status.HTTP_404_NOT_FOUND)
-            serialized_data = self.serializer_class(cases, many=True).data
-            return Response(serialized_data, status=status.HTTP_200_OK)
-        except Exception as e:
-            print(e)
-            return Response({"An unexpected error occurred. Please try again later."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 
