bcc3fd001c784cd039f0fbf1db45b5a7b678603c
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2025-04-08 21:32:10 +0700

    [REFACTOR] Update URL path for StatisticsView to improve API structure

--- pt_backend/urls.py
+++ pt_backend/urls.py
@@ -4,6 +4,6 @@ from .views import AllCaseLocationsView, FiltersView, StatisticsView
 urlpatterns = [
     path('cases/locations/', AllCaseLocationsView.as_view(), name='all-case-locations'),
     path('api/filters/', FiltersView.as_view(), name='filters'),
-    path('api/dashboard/disease-case-info/', StatisticsView.as_view(), name='statistics'),
+    path('api/statistics/', StatisticsView.as_view(), name='statistics'),
 ]
 
