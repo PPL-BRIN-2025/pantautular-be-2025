474ac3cd71d31abf6e2c8e894f89b1e3d5506e3b
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-04-08 15:06:02 +0700

    Update statistics.py, make SeverityGroupingReport the same with other methods

--- pt_backend/statistics.py
+++ pt_backend/statistics.py
@@ -4,18 +4,7 @@ from datetime import datetime
 
 class SeverityGroupingReport:
  
-    def __init__(self, case_filter_service):
-        self.case_filter_service = case_filter_service
-
-    def generate_report(self, provinces=None, cities=None, news_portals=None, severities=None, news_date_range=None):
-        filtered_cases = self.case_filter_service.filter_cases(
-            provinces=provinces,
-            cities=cities,
-            news_portals=news_portals,
-            severities=severities,
-            news_date_range=news_date_range,
-        )
-        
+    def generate_report(self, filtered_cases):
         severity_counts = Counter()
         total_cases = 0
         
