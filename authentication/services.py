9d6048a12efe8367d63a75b6140540285dc71f34
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-04-27 16:14:44 +0700

    Update services.py

--- authentication/services.py
+++ authentication/services.py
@@ -1,3 +1,4 @@
+# from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -6,16 +7,17 @@ from pt_backend.models import User
 from django.contrib.auth.hashers import make_password
 from .repository import UserRepository
 
-import os
-
 class PasswordResetService:
-    def __init__(self, reset_url_base=None):
-        self.reset_url_base = reset_url_base or os.getenv(
-            'PROD_PASSWORD_RESET_URL') or os.getenv(
-            'DEV_PASSWORD_RESET_URL')
+    def __init__(self, reset_url_base="http://localhost:3000/authentication/reset-password"):
+        self.reset_url_base = reset_url_base
+    
+    @staticmethod
+    def get_user_model():
+        return User
     
     def find_user_by_email(self, email):
-        return User.objects.get(email=email) if User.objects.filter(email=email).exists() else None
+        user_model = self.get_user_model()
+        return user_model.objects.get(email=email) if user_model.objects.filter(email=email).exists() else None
     
     def generate_password_reset_token(self, user):
         uid = urlsafe_base64_encode(force_bytes(user.pk))
@@ -43,6 +45,7 @@ class PasswordResetService:
     
 
 
+
 class ChangePasswordService:
     def __init__(self, repository: UserRepository = UserRepository()):
         self.repository = repository
@@ -55,19 +58,3 @@ class ChangePasswordService:
         user.password = make_password(new_password)
         self.repository.save_user(user)
         return True
-=======
-    def get_user_from_uidb64(self, uidb64):
-        """Decode uidb64 and retrieve the user"""
-        try:
-            uid = urlsafe_base64_decode(uidb64).decode()
-            user = User.objects.get(pk=uid)
-            return user
-        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-            return None
-    
-    def validate_token(self, user, token):
-        """Validate if the token is valid for the given user"""
-        if not user:
-            return False
-        return default_token_generator.check_token(user, token)
-
