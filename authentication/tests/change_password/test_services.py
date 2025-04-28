6bc8a0102ba3c53289d90b14fd8b077b28ab5b9a
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2025-04-28 08:18:57 +0700

    [CHORES] still trying to fix test detected as risk

--- authentication/tests/change_password/test_services.py
+++ authentication/tests/change_password/test_services.py
@@ -74,5 +74,5 @@ class UpdateUserPasswordTest(TestCase):
         
         self.user.refresh_from_db()
         self.assertFalse(result["success"])
+        self.assertTrue(check_password("current_password", self.user.password)) # NOSONAR - test data
         self.assertFalse(check_password("new_secure_password", self.user.password)) # NOSONAR - test data
-        self.assertTrue(check_password("current_password", self.user.password)) # NOSONAR - test data only
