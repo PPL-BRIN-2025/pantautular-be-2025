5de2d36eeb58966514f2eca500a0d6e06f9c9f88
alvaro.luqman1 <alvaro.luqman@gmail.com>
2025-05-04 18:36:32 +0700

    [FEATURE] Add serializers for province climate data and update views to use new serializers

--- pt_backend/serializers.py
+++ pt_backend/serializers.py
@@ -53,3 +53,66 @@ class LocationSeverityStatsSerializer(serializers.Serializer):
 class ProvinceClimateValueSerializer(serializers.Serializer):
     id = serializers.CharField()
     value = serializers.DecimalField(max_digits=8, decimal_places=2)
+
+# Map of Indonesian province names to ISO 3166-2 codes
+PROVINCE_TO_CODE = {
+    'Aceh': 'ID-AC',
+    'Bali': 'ID-BA',
+    'Bangka Belitung': 'ID-BB',
+    'Banten': 'ID-BT',
+    'Bengkulu': 'ID-BE',
+    'DI Yogyakarta': 'ID-YO',
+    'DKI Jakarta': 'ID-JK',
+    'Gorontalo': 'ID-GO',
+    'Jambi': 'ID-JA',
+    'Jawa Barat': 'ID-JB',
+    'Jawa Tengah': 'ID-JT',
+    'Jawa Timur': 'ID-JI',
+    'Kalimantan Barat': 'ID-KB',
+    'Kalimantan Selatan': 'ID-KS',
+    'Kalimantan Tengah': 'ID-KT',
+    'Kalimantan Timur': 'ID-KI',
+    'Kalimantan Utara': 'ID-KU',
+    'Kepulauan Riau': 'ID-KR',
+    'Lampung': 'ID-LA',
+    'Maluku': 'ID-MA',
+    'Maluku Utara': 'ID-MU',
+    'Nusa Tenggara Barat': 'ID-NB',
+    'Nusa Tenggara Timur': 'ID-NT',
+    'Papua': 'ID-PA',
+    'Papua Barat': 'ID-PB',
+    'Riau': 'ID-RI',
+    'Sulawesi Barat': 'ID-SR',
+    'Sulawesi Selatan': 'ID-SN',
+    'Sulawesi Tengah': 'ID-ST',
+    'Sulawesi Tenggara': 'ID-SG',
+    'Sulawesi Utara': 'ID-SA',
+    'Sumatera Barat': 'ID-SB',
+    'Sumatera Selatan': 'ID-SS',
+    'Sumatera Utara': 'ID-SU'
+}
+
+class BaseProvinceClimateSerializer(serializers.Serializer):
+    province = serializers.CharField()
+    value = serializers.DecimalField(max_digits=8, decimal_places=2)
+    
+    def to_representation(self, instance):
+        # Get province name from the instance data structure
+        province_name = instance.get('province', None)
+        if province_name is None:
+            province_name = instance.get('id', '')
+        
+        # Return data with province converted to ISO code
+        return {
+            'id': PROVINCE_TO_CODE.get(province_name, province_name),
+            'value': float(instance.get('value', 0))
+        }
+
+class ProvinceHumiditySerializer(BaseProvinceClimateSerializer):
+    pass
+
+class ProvinceTemperatureSerializer(BaseProvinceClimateSerializer):
+    pass
+
+class ProvincePrecipitationSerializer(BaseProvinceClimateSerializer):
+    pass
