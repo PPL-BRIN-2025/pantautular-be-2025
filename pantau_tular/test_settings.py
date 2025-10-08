a88c93752de4e21b6b7c11f5d521daadacadddcb
AndhikaNayakaAW <andhika.nayaka@ui.ac.id>
2025-10-08 19:46:48 +0700

    [REFACTOR] Add test settings and SQLite database for local testing

---
+++ pantau_tular/test_settings.py
@@ -0,0 +1,22 @@
+import os
+
+from .settings import *  # noqa: F401,F403
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "test_db.sqlite3",
+    }
+}
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+SECRET_API_KEYS = [os.getenv("SECRET_API_KEY", "test-api-key")]
+
+CAPTCHA_ENABLED = False
