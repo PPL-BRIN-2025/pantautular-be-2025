df2e7b798ce3b75431ccce2f4fdbc6e6efc0de46
Kevin Adriano <kevin.adriano@ui.ac.id>
2025-10-29 15:57:38 +0700

    [REFACTOR] Move validation + database construction into CaseWriteSerializer

---
+++ expert_user_feature/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from pt_backend.models import Case, Disease, Location, News
+
+
+class CaseWriteSerializer(serializers.Serializer):
+    disease = serializers.CharField()
+    gender = serializers.CharField()
+    age = serializers.IntegerField()
+    city = serializers.CharField()
+    status = serializers.CharField()
+    severity = serializers.CharField()
+    location = serializers.DictField()
+    news = serializers.DictField()
+
+    def create(self, validated_data):
+        location_data = validated_data.pop("location")
+        news_data = validated_data.pop("news")
+
+        disease = Disease.objects.get(name=validated_data["disease"])
+
+        location, _ = Location.objects.get_or_create(
+            city=location_data.get("city"),
+            defaults={
+                "province": location_data.get("province"),
+                "latitude": location_data.get("latitude"),
+                "longitude": location_data.get("longitude"),
+            },
+        )
+
+        case = Case.objects.create(disease=disease, location=location, **validated_data)
+
+        News.objects.create(case=case, **news_data)
+        return case
--- expert_user_feature/views.py
+++ expert_user_feature/views.py
@@ -1,58 +1,13 @@
-from rest_framework.views import APIView
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework import status
 
-from pt_backend.models import Case, Disease, Location, News
 from authentication.security import CustomJWTAuthentication
 from authentication.permissions import IsTokenAuthenticated
 from .permissions import IsExpertUserRole
+from .serializers import CaseWriteSerializer
 
 
-class ExpertAddCaseBadView(APIView):
+class ExpertAddCaseRefactoredView(generics.CreateAPIView):
     authentication_classes = [CustomJWTAuthentication]
     permission_classes = [IsTokenAuthenticated, IsExpertUserRole]
-
-    def post(self, request):
-        data = request.data
-
-        # Inline lookups (hard-coded logic)
-        disease = Disease.objects.filter(name=data.get("disease")).first()
-        if not disease:
-            return Response({"error": "Disease not found"}, status=status.HTTP_400_BAD_REQUEST)
-
-        location_data = data.get("location", {})
-        location = Location.objects.filter(
-            city=location_data.get("city")
-        ).first()
-        if not location:
-            location = Location.objects.create(
-                city=location_data.get("city"),
-                province=location_data.get("province"),
-                latitude=location_data.get("latitude"),
-                longitude=location_data.get("longitude"),
-            )
-
-        case = Case.objects.create(
-            disease=disease,
-            location=location,
-            gender=data.get("gender"),
-            age=data.get("age"),
-            city=data.get("city"),
-            status=data.get("status"),
-            severity=data.get("severity"),
-        )
-
-        news_data = data.get("news", {})
-        News.objects.create(
-            case=case,
-            portal=news_data.get("portal"),
-            title=news_data.get("title"),
-            type=news_data.get("type"),
-            content=news_data.get("content"),
-            url=news_data.get("url"),
-            author=news_data.get("author"),
-            date_published=news_data.get("date_published"),
-            img_url=news_data.get("img_url", ""),
-        )
-
-        return Response({"message": "Case created"}, status=status.HTTP_201_CREATED)
+    serializer_class = CaseWriteSerializer
